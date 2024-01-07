1e6ad521ad9651b38f95fc0234e3a7f0b71c3202
kipla <kiplagatbrian18@gmail.com>
2024-01-07 15:25:14 +0300

    Add dynamic messages

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Container, Row, Col} from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import cicd from "../../Assets/download.jpeg";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
@@ -10,87 +10,53 @@ import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
-  return (
-    <Container fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Pixcoin"
-              description="A crypto P2P App for buyers and sellers to trade Bitcoin with escrow. Users can login, open a trade and parties can chat and trade BTC for cash. Built with Angular.js, Laravel Lumen APIs, and Firebase SDK. Have features which allows user for realtime messaging, image sharing as well as supports deposits and withdrawals!"
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://pixcoinn.com"
-            />
-          </Col>
+   const [data,setData] = useState(null)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:7AzsX1rL/projects')
+                const result = await response.json();
+                setData(result);
+                console.log(result)
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="CI-CD Pipeline"
-              description="I got bored deploying react code on vercel. I wrote a CI-CD Workflow to automate deployment to my Linux VM using github actions. I setup a Github Hosted runner and wrote a custom YAML File to enable the runner build, test, SSH into and automatically deploy the app on my RHEL Centos 7 VM."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+            } catch (e) {
+                console.log("Error",e)
+            }
+        }
+        fetchData()
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
+    }, []);
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
+    return (
+        <Container fluid className="project-section">
+            <Particle/>
+            <Container>
+                <h1 className="project-heading">
+                    My Recent <strong className="purple">Works </strong>
+                </h1>
+                <p style={{color: "white"}}>
+                    Here are a few projects I've worked on recently.
+                </p>
+                <Row style={{justifyContent: "center", paddingBottom: "10px"}}>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
+                    {data && data.map((project, index) => (
+                        <Col md={4} className="project-card" key={index}>
+                            <ProjectCard
+                                imgPath={project.imgPath}
+                                isBlog={project.isBlog}
+                                title={project.title}
+                                description={project.description}
+                                ghLink={project.ghLink}
+                                demoLink={project.demoLink}
+                            />
+                        </Col>
+                    ))}
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
-        </Row>
-      </Container>
-    </Container>
-  );
+
+                </Row>
+            </Container>
+        </Container>
+    );
 }
 
 export default Projects;
